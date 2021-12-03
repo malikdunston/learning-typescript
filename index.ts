e96309a3b21b8343bdff1f9d1b014c8d9055dc81
Malik Dunston <hello@malikdunston.com>
2021-12-03 07:45:41 -0500

    more practice

--- index.ts
+++ index.ts
@@ -1,3 +1,21 @@
-async function getData(){
-	return "abc";
+function pow( x: number, y: number): void{
+	return Math.pow(x, y).toString();
 }
+
+pow(5, 10);
+
+const arr: number[] = [];
+
+arr.push(1);
+arr.push("23"); //error
+arr.push(false); //error
+
+// tuples...
+
+type myList = [number, string, boolean]
+
+const arr2: myList = []
+
+arr2.push(1);
+arr2.push("23"); //error
+arr2.push(false); //error

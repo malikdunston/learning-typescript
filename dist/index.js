f08600afbcdfadf1709d91271e8c0f5967eebb0a
Malik Dunston <hello@malikdunston.com>
2021-12-05 23:50:51 -0500

    basic types...

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,15 @@
 let id = 5;
 // tsc -- watch [file]
 // you can do above in tsconfig.json...
+console.log("id: ", id);
+// basic types
+let company = "Traversy Media";
+let isPublished = true;
+let x = "hello";
+x = true; // no problem bc any type...
+let age;
+age = 30;
+let ids = [1, 2, 3, 4, 5]; // array with only numbers....
+// ids.push("hello") // error...
+let arr = [1, true, "hello"];
+// Tuples
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,23 @@ let id: number = 5;
 // tsc -- watch [file]
 // you can do above in tsconfig.json...
 
+console.log("id: ", id)
+
+
+// basic types
+
+let company: string = "Traversy Media";
+let isPublished: boolean = true;
+let x: any = "hello";
+
+x = true; // no problem bc any type...
+
+let age: number;
+age = 30;
+
+let ids: number[] = [1, 2, 3, 4, 5] // array with only numbers....
+// ids.push("hello") // error...
+
+let arr: any[] = [1, true, "hello"];
+
+// Tuples
